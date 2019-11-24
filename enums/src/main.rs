b1f7d9689d6f373044b8791dfc52c4369dfb8a1d
justin cechmanek <gj.cechmanek@gmail.com>
2019-11-23 19:48:54 -0800

    simple match keyword example

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -13,6 +13,37 @@ fn route(ip_kind: &IpAddress) {
     // empty function 
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+// this function takes immutable reference to coin
+fn print_value_in_cents(coin: &Coin) {
+    let value = match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    };
+    println!("value is {}", value);
+}
+
+// this function takes ownership of coin
+fn get_value_in_cents(coin: Coin) -> u8 {
+    let value = match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25, 
+        // all enum possibilities must exist. 
+        //comment out one line and compiler complains
+    };
+    return value;
+}
+
 fn main() {
     let home = IpAddress::V4(127,0,0,1);
     
@@ -25,4 +56,14 @@ fn main() {
     // calling methods of enums the same as we do from structs
     home.route();
     loopback.route();
+
+    // enums combined with 'match' keyword
+    let my_coin = Coin::Nickel;
+
+    print_value_in_cents(&my_coin);
+
+    let value = get_value_in_cents(my_coin);
+
+    println!("getting value of my coin: {}", value);
+
 }
