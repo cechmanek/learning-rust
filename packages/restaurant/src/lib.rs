8869d9d4224b7fd86d9554929900a767127142d6
justin cechmanek <gj.cechmanek@gmail.com>
2019-11-26 20:26:00 -0800

    moves front_of_house and back_of_house modules to their own files

--- packages/restaurant/src/lib.rs
+++ packages/restaurant/src/lib.rs
@@ -1,50 +1,6 @@
-mod front_of_house { // declare the 'front_of_house' module
-    pub mod hosting { // declare the' front_of_house::hosting' sub module
-        pub fn add_to_waitlist() {}
+mod front_of_house; // implementation of front_of_house module is is 'front_of_house.rs'
 
-        fn seat_at_table() {}
-    }
-
-    mod serving { // declare the' front_of_house::serving' sub module
-        fn take_order() {}
-        
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order(); // calls parent module function, not serving::serve_order()
-    }
-
-    fn cook_order() {}
-
-    pub struct Breakfast { // public struct. this can be instantiated from outside
-        pub toast: String, // public struct member. this can be set and seen from outside
-        seasonal_fruit: String, // private struct member. this can't be set or seen from outside
-    }
-
-    impl Breakfast {
-        pub fn summer (toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"), // private members can still be constructed
-            }
-        }
-
-    }
-    
-    pub enum Appetizer { // if an enum is made public then all of it's members are public
-        Soup,
-        Salad,
-    }
-
-}
-
-fn serve_order() {} // same name as serving::serve_order(), but it's in a parent module so it's ok
+mod back_of_house;
 
 fn eat_at_restuarant() {
     // call function of module by specifying absolute path
