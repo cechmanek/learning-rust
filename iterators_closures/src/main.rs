7b955f9f56ae28659d32a3463eba83c2db68c88c
justin cechmanek <gj.cechmanek@gmail.com>
2020-06-15 10:53:28 -0700

    adds iterator examples

--- iterators_closures/src/main.rs
+++ iterators_closures/src/main.rs
@@ -102,9 +102,85 @@ fn generate_workout_better_closure(intensity: u32, random_number: u32) {
         }
     }
 
+
+
 }
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
+
+    // below are some examples of using iterators
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val); // here val is type *int, aka only a refernce
+    } // for loop implicitly takes ownership, but val still isn't mutable
+
+    // we can't re-use the same iterator, we need to make a new one,
+    // but this is free computation, so don't worry about it 
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1)); // .next() returns immutable reference
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3)); 
+    assert_eq!(v1_iter.next(), None); // returns None when we get to end of container
+
+    // if we want to take ownership use v1.into_iter()
+    let v1_in_iter = v1.into_iter();
+    for mut val in v1_in_iter {
+        val += 1; // val is now type int
+    }
+
+    // if we want mutable references us v1.iter_mut(), just remember that container must be mutable
+    let mut v2 = vec![1,2,3];
+    let v2_it_mut = v2.iter_mut();
+    for val in v2_it_mut {
+        *val = *val  + 1; // val is a reference, so need to dereference with *
+    }
+
+    // iterators get used up when called, but only when consumed by something
+    let v3 = vec![1,2,3];
+    let v3_iter = v3.iter();
+    let total: i32 = v3_iter.sum(); // sum() is a collector that consumes an iterator
+    println!("the sum of [1,2,3] is {}",total);
+
+    // another collection example
+    let v4 = vec![1,2,3];
+    let v4_iter = v4.iter();
+    let plus1: Vec<_> = v4_iter.map(|x| x+1).collect();
+    // map() returns another iterator, collect() is a collector that consumes an iterator
+    assert_eq!(plus1, vec![2,3,4]);
+
+    // yet another collection, filters
+    let v5 = vec![1,2,3,4,5,6,7,8,9,10];
+    let v5_iter = v5.into_iter(); // use into_iter so we don't have to dereference
+    let evens: Vec<_> = v5_iter.filter(|x| (x%2)==0).collect();
+    assert_eq!(evens, vec![2,4,6,8,10]);
+
+    // use our custom Counter class with its iterator methods
+}
+
+// to create our own iterator we need to implement the iterator trait, which only needs .next()
+struct Counter {
+    count: u32,
+}
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0}
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            return Some(self.count);
+        }
+        else {
+            return None;
+        }
+    }
 
 }
