0edde03ca07fe0c2104a2739ee629779b90cf42b
justin cechmanek <gj.cechmanek@gmail.com>
2020-06-18 13:28:15 -0700

    finishes chapter 14, cargo and crates package repository

--- crates_and_cargo/src/main.rs
+++ crates_and_cargo/src/main.rs
@@ -1,13 +1,4 @@
-/// Main function in program
-/// 
-/// This is a documenation comment that is annotated with 3 backslashes '///'
-/// 
-/// It supports `Markdown`
-/// run >> cargo doc to generate html documentation
-/// # like this heading
-/// ## and this sub heading
-/// 
-/// run >> cargo doc --open to view the html in your browswer
+
 fn main() {
     println!("Hello, world!");
 }
