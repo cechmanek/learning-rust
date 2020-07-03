dce2df6977ec96e0bc01379e968a4851ab6a4bbd
justin cechmanek <gj.cechmanek@gmail.com>
2020-07-03 14:37:34 -0700

    adds examples of using unsafe rust

---
+++ advanced_structures/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+  // example of unsafe rust super powers
+  // 1) dereferencing raw pointers  
+  let mut num = 5;
+
+  // create raw pointer by casting regular Rust pointers to their raw types
+  let r1 = &num as *const i32; // *const is the full name, the asterisc is part of type name
+  let r2 = &mut num as *mut i32; // *mut is full name 
+
+  // example of raw memory access
+  let address = 0x012345usize; // some random hardcoded access
+  let r = address as *const i32;
+
+  unsafe { // can only dereference these r1 r2 pointers inside an unsafe{} block
+    println!("r1 is: {}", *r1);
+    println!("r2 is: {}", *r2);
+  }
+
+  unsafe {
+    dangerous(); // can only be called inside an unsafe{} code block
+  }
+
+}
+
+// defining unsafe functions is identical to regular functions, except with the unsafe keyword
+unsafe fn dangerous() {
+  // do some unsafe actions. no need to add an usafe{} block. this whole function is an unsafe block
+}
+
+// we can have regular, safe, functions that have internal unsafe{} blocks
+fn split_at_index(my_arr: &mut [i32], mid: usize) ->(&mut[i32], &mut[i32]) {
+  // accept an array and index, return two arrays split at the index
+  let len = my_arr.len();
+
+  assert!( mid <= len);
+  // return (&mut slice[..mid], &mut slice[mid..]); // won't compile as we have 2 borrows
+  // even though we as programers know the mutable slices don't overlap, the compiler doesn't
+
+  use std::slice;
+  let ptr = my_arr.as_mut_ptr(); // raw pointer. we can access safely, but can't dereference
+  unsafe {
+    let a = (slice::from_raw_parts_mut(ptr, mid),
+             slice::from_raw_parts_mut(ptr.add(mid), len - mid)
+            );
+    return a;
+  }
+}
