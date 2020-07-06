b6d44657ea73b8de9fd8c908c29de3717d8f2ffd
justin cechmanek <gj.cechmanek@gmail.com>
2020-07-05 17:06:08 -0700

    adds Worker struct to threadpool that can live without running tasks

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,1 +1,47 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+  workers : Vec<Worker>,
+}
+
+impl ThreadPool {
+  /// Create a new ThreadPool.
+  ///
+  /// The size is the number of threads in the pool.
+  ///
+  /// # Panics
+  ///
+  /// The `new` function will panic if the size is zero.
+  pub fn new(size: usize) -> ThreadPool {
+    assert!(size > 0); // doesn't make sense to have 0 threads
+
+    let mut workers = Vec::with_capacity(size);
+
+    for id in 0..size {
+      workers.push(Worker::new(id));
+    }
+
+
+    return ThreadPool{ workers };
+  }
+
+  pub fn execute<F>(&self, f: F) // define to take a closure as parameter
+    where
+      F: FnOnce() + Send + 'static, // FnOnce is one of three possible traits to use
+    {
+
+    }
+}
+
+// Worker is our light wrapper around a thread
+struct Worker {
+  id: usize,
+  thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+  pub fn new(id: usize) -> Worker {
+    let thread = thread::spawn(|| {}); // spawn with empty code block. needed to keep alive
+    return Worker { id, thread };
+  }
+}
