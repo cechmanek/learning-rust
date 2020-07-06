2728aa55b286ae1d4bb817d67df002184ac89bcf
justin cechmanek <gj.cechmanek@gmail.com>
2020-07-05 17:14:03 -0700

    uses atomic ref counting and mutex lock to share receiver among threads

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,7 +1,11 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 pub struct ThreadPool {
   workers : Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -14,23 +18,25 @@ impl ThreadPool {
   /// The `new` function will panic if the size is zero.
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0); // doesn't make sense to have 0 threads
+    
+    let (sender, receiver) = mpsc::channel();
+
+    let receiver = Arc::new(Mutex::new(receiver));
 
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
-      workers.push(Worker::new(id));
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
-
-
-    return ThreadPool{ workers };
+    return ThreadPool{ workers, sender };
   }
 
   pub fn execute<F>(&self, f: F) // define to take a closure as parameter
     where
       F: FnOnce() + Send + 'static, // FnOnce is one of three possible traits to use
     {
-
     }
+
 }
 
 // Worker is our light wrapper around a thread
@@ -40,8 +46,13 @@ struct Worker {
 }
 
 impl Worker {
-  pub fn new(id: usize) -> Worker {
-    let thread = thread::spawn(|| {}); // spawn with empty code block. needed to keep alive
+  pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(|| {
+      receiver;
+    }); // spawn with empty code block. needed to keep alive
+    
     return Worker { id, thread };
   }
 }
+
+struct Job;
